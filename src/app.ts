04c60782a6076b2009658d31e3d31945cf996acf
Abhishek Kumar <abhi24033c@gmail.com>
2024-05-12 12:07:51 +0530

    integrate global http-errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import  express  from "express";
+import { globalErrorHandler } from "./middleware/globalErrorHandler";
+import createHttpError from "http-errors";
 
 const app= express();
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(globalErrorHandler);
 // routes
 app.get("/", (req, res) => {
+  const stayus=createHttpError(400,"Something went wrong");
+  throw stayus;
   res.send("Hello World!");
 });
 
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ conf();
 const _config={
     port:process.env.PORT,
     mongouri:process.env.MONGOURI,
+    env:process.env.NODE_ENV,
 }
 
 export const config=Object.freeze(_config);
