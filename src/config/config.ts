d112de0b5ec63f968c327c8bb62b4173faa956e3
Abhishek Kumar <abhi24033c@gmail.com>
2024-05-12 15:29:25 +0530

    Add jwt

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,7 @@ const _config={
     port:process.env.PORT,
     mongouri:process.env.MONGOURI,
     env:process.env.NODE_ENV,
+    jwtSecretKey:process.env.JWT_SECRET_KEY
 }
 
 export const config=Object.freeze(_config);
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,23 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import usermodel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+// import { sign } from "jsonwebtoken";
 
-export const userRegistration = async(req:Request, res:Response,next:NextFunction) => {
-    const {name,email,password}=req.body;
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name, email, password } = req.body;
 
-    // validation
-    if(!name || !email ||!password){
-        const error=createHttpError(400,"All fields are required");
-        return next(error);
-    }
-    // check user is alredy regisered or not
-    const user=await usermodel.findOne({email});
-    if(user){
-        const error=createHttpError(400,"User is already registered");
-        return next(error);
-    }
-    
-    // process
-    // responce
-  res.send("User registerd");
+  // validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  // check user is alredy regisered or not
+  const user = await usermodel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User is already registered");
+    return next(error);
+  }
+  // password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await usermodel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  // generate token
+  const token = sign({sub:newUser?._id },config.jwtSecretKey as string, {expiresIn:"7d"});
+
+  // process
+  // responce
+  res.json({ accessToken:token });
 };
